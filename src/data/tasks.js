ea385f7600ab7d6243355bdb5c5ddb24b5b9f343
Marco Bahamondes <mbahamondes.osorio@gmail.com>
2023-04-03 15:21:54 -0400

    primer commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'Mi primer tarea',
+        description: 'Mi primer tarea desc.'
+    },
+    {
+        id: 1,
+        title: 'Mi segunda tarea',
+        description: 'Mi segunda tarea desc.'
+    },
+    {
+        id: 2,
+        title: 'Mi tercera tarea',
+        description: 'Mi tercera tarea desc.'
+    }
+]
